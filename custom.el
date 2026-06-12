3ba85d14206d914296618ed9b243659be15bbd35
Sarika Chaudhary <sarika.csrd@gmail.com>
2026-06-12 09:55:41 +0100

    Initial Doom Emacs Config

---
+++ custom.el
@@ -0,0 +1,16 @@
+;;; -*- lexical-binding: t -*-
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(org-agenda-files
+   '("~/Library/CloudStorage/OneDrive-UniversityofEastAnglia/1-academic/5-self-learning/5-programming/emacs/notes.org"))
+ '(package-selected-packages '(ayu-theme ess-R-data-view markdown-mode))
+ '(warning-suppress-log-types '((ox-latex))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
